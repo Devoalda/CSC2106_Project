f6e32ec4b0f29d7ca8914abd83829b968fede82a
devoalda <linuxes_mojoworld@aleeas.com>
2024-03-03 15:40:00 +0800

    Initial Commit: ESP-Now Baseline

---
+++ ESP-Now-Mesh/ESP-Now-Mesh.ino
@@ -0,0 +1,162 @@
+#include <WiFi.h>
+#include <esp_now.h>
+
+void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength)
+// Formats MAC Address
+{
+  snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+}
+
+void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen)
+// Called when data is received
+{
+  // Only allow a maximum of 250 characters in the message + a null terminating byte
+  char buffer[ESP_NOW_MAX_DATA_LEN + 1];
+  int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
+  strncpy(buffer, (const char *)data, msgLen);
+
+  // Make sure we are null terminated
+  buffer[msgLen] = 0;
+
+  // Format the MAC address
+  char macStr[18];
+  formatMacAddress(macAddr, macStr, 18);
+
+  // Send Debug log message to the serial port
+  Serial.printf("Received message from: %s - %s\n", macStr, buffer);
+
+  // Add an additional message to the buffer
+  char additionalMessage[] = "Test Extra Message";
+  strncat(buffer, additionalMessage, sizeof(additionalMessage));
+
+  // Add the received MAC address into the peer list if it doesn't exist
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, macAddr, 6);  // Copy MAC address
+  peerInfo.channel = 0;                    // Use the default channel
+  peerInfo.encrypt = false;                // No encryption for simplicity
+  if (!esp_now_is_peer_exist(macAddr)) {
+    esp_now_add_peer(&peerInfo);
+    Serial.println("Added peer to the list");
+  } else {
+    Serial.println("Peer already exists in the list");
+  }
+
+  esp_now_peer_num_t peer_num;
+  esp_now_get_peer_num(&peer_num);
+
+  // Loop through the peer list and send the message to each peer except the sender
+  for (int i = 0; i < peer_num.total_num; i++) {
+    if (esp_now_fetch_peer(i, &peerInfo) == ESP_OK) {
+      if (memcmp(peerInfo.peer_addr, macAddr, 6) != 0) {  // Check if not the sender
+        esp_err_t result = esp_now_send(peerInfo.peer_addr, (const uint8_t *)buffer, strlen(buffer));
+
+        // Print results to serial monitor
+        if (result == ESP_OK) {
+          Serial.printf("Forwarded message: %s to: %02X:%02X:%02X:%02X:%02X:%02X\n", buffer,
+                        peerInfo.peer_addr[0], peerInfo.peer_addr[1], peerInfo.peer_addr[2],
+                        peerInfo.peer_addr[3], peerInfo.peer_addr[4], peerInfo.peer_addr[5]);
+        } else {
+          Serial.println("Error sending message to peer");
+        }
+      } else {
+        // Serial.println("Sender!");
+      }
+    }
+  }
+}
+
+
+
+
+
+void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status)
+// Called when data is sent
+{
+  // char macStr[18];
+  // formatMacAddress(macAddr, macStr, 18);
+  // Serial.print("Last Packet Sent to: ");
+  // Serial.println(macStr);
+  // Serial.print("Last Packet Send Status: ");
+  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void broadcast(const String &message)
+// Emulates a broadcast
+{
+  // Broadcast a message to every device in range
+  uint8_t broadcastAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+  esp_now_peer_info_t peerInfo = {};
+
+  memcpy(&peerInfo.peer_addr, broadcastAddress, 6);
+  if (!esp_now_is_peer_exist(broadcastAddress)) {
+    esp_now_add_peer(&peerInfo);
+  }
+  // Send message
+  esp_err_t result = esp_now_send(broadcastAddress, (const uint8_t *)message.c_str(), message.length());
+
+  // Print results to serial monitor
+  if (result == ESP_OK) {
+    Serial.println("Broadcast message success");
+  } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+    Serial.println("ESP-NOW not Init.");
+  } else if (result == ESP_ERR_ESPNOW_ARG) {
+    Serial.println("Invalid Argument");
+  } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+    Serial.println("Internal Error");
+  } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+    Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+  } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+    Serial.println("Peer not found.");
+  } else {
+    Serial.println("Unknown error");
+  }
+}
+
+String getRandomFloatAsString(float min, float max) {
+  // Generate a random floating-point number
+  float randomFloat = min + random() / ((float)RAND_MAX / (max - min));
+
+  // Convert the float to a string
+  char buffer[10];                     // Adjust the size as needed
+  dtostrf(randomFloat, 6, 2, buffer);  // Format the float with 6 total characters and 2 decimal places
+  String floatString = String(buffer);
+
+  return floatString;
+}
+
+void setup() {
+
+  // Set up Serial Monitor
+  Serial.begin(115200);
+  delay(1000);
+
+  // Set ESP32 in STA mode to begin with
+  WiFi.mode(WIFI_STA);
+  Serial.println("ESP-NOW Broadcast Demo");
+
+  // Print MAC address
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+
+  // Disconnect from WiFi
+  WiFi.disconnect();
+
+  // Initialize ESP-NOW
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESP-NOW Init Success");
+    esp_now_register_recv_cb(receiveCallback);
+    esp_now_register_send_cb(sentCallback);
+  } else {
+    Serial.println("ESP-NOW Init Failed");
+    delay(3000);
+    ESP.restart();
+  }
+
+  float min = 0.0;
+  float max = 100.0;
+
+  broadcast(getRandomFloatAsString(min, max));
+}
+
+void loop() {
+}
